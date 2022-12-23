9cdf51a8c8b106718f8e1c25c1079636284f9afd
njj <njj@laufire.in>
2022-12-23 10:49:26 +0530

    Introduced a IncreaceCount.

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
+  const [ state, setState ] = useState({ count: 0 });
+
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Open up App.js to start working on your app!</Text>
       <StatusBar style="dark" />
+      <Text>{state.count}</Text>
+      <Button title='IncreaceCount' color={'red'} onPress={() => setState({ count: state.count + 1 })}/>
     </View>
   );
 }
