6c78ef288911cc2271ba6ce1151ea4b54e914037
nirupan <nirupanjoej@gmail.com>
2022-12-28 12:36:27 +0530

    Refactored.

--- components/index.js
+++ components/index.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import {  View } from "react-native";
-import useStyles from "../styles"
+import useStyles from "./styles"
 import Theme from "./Theme";
 import Contact from "./view/contact";
 
