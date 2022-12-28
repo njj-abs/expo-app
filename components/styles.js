7feb5c5beb12def52bd44fe0593fc780b29c6256
nirupan <nirupanjoej@gmail.com>
2022-12-28 13:53:29 +0530

    Improved some style.

--- components/styles.js
+++ components/styles.js
@@ -1,15 +1,20 @@
 import { makeStyles } from "@rneui/themed";
 
 const useStyles = makeStyles((theme) => ({
-    container: {
-      flex: 1,
-      backgroundColor: theme.colors.background,
-      alignItems: "center",
-      justifyContent: "center",
-    },
-    text: {
-      marginVertical: theme.spacing.lg,
-    },
-  }));
+  container: {
+    flex: 1,
+    backgroundColor: theme.colors.background,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    marginVertical: theme.spacing.lg,
+  },
+  contact: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    margin: 30,
+  },
+}));
 
 export default useStyles
--- components/view/contact.js
+++ components/view/contact.js
@@ -1,10 +1,11 @@
 import { Text } from "@rneui/themed";
+import { View } from "react-native";
 import ContactButton from "./contactButton";
 
 const Contact = (context) =>
-  <>
+  <View style={ context.styles.contact}>
     <ContactButton { ...context }></ContactButton>
     <Text>{context.state.contacts.length}</Text>
-  </>;
+  </View>;
 
 export default Contact
