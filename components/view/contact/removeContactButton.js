d538909b74c629d508053beba1ab4ad8fd26f1aa
nirupan <nirupanjoej@gmail.com>
2023-01-05 13:29:55 +0530

    Created the removeContact.

--- components/view/contact/removeContactButton.js
+++ components/view/contact/removeContactButton.js
@@ -2,24 +2,24 @@ import { Button } from "@rneui/themed";
 import * as Contacts from 'expo-contacts';
 
 const removeContact = async (context) => {
-    console.warn(context.state.contacts);
-    console.warn(context.state.remove,'remove');
+    console.warn(context.state.contacts[0]);
+    const contact = {
+        [Contacts.Fields.FirstName]: 'A',
+        [Contacts.Fields.LastName]: 'Man',
+        [Contacts.Fields.Company]: 'Young Money',
+        [Contacts.Fields.PhoneNumbers]: [
+            {
+                id: String(Math.random()).slice(2),
+                label: 'person',
+                countryCode: '+91',
+                number: '1234567890',
+            }
+        ]
+    };
+    const contactId = await Contacts.removeContactAsync(context.state.contacts[0].id);
 
-    console.warn(context.state.contacts.filter(({firstName} ) => firstName === context.state.remove), 'filer');
-//     context.state.contacts.find(({phoneNumbers}) => 
-//     phoneNumbers.some(({number}) => number === Number(context.state.remove))
-// );
-    const [{lookupKey}] = context.state.contacts.filter(({firstName} ) => firstName === context.state.remove);
+    console.warn(contactId);
 
-    console.warn(lookupKey);
-    await Contacts.removeContactAsync(lookupKey)
-        .then((contactId) => {
-            alert("removed successfully");
-        })
-        .catch((err) => {
-            alert(err);
-            console.log(err);
-        });
 };
 
 const RemoveContactButton = (context) =>
