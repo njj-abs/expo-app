3e12f9708bbce994d07097a1fd5aff62026fa5cd
nirupan <nirupanjoej@gmail.com>
2023-01-05 17:15:20 +0530

    Improved the removeContact.

--- components/view/contact/removeContactButton.js
+++ components/view/contact/removeContactButton.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Button } from '@rneui/themed';
 import * as Contacts from 'expo-contacts';
+import RemoveHelper from "../../../services/RemoveHelper";
 
 const removeContact = async (context) => {
 	console.warn(context.state.contacts[0]);
 
-	const contactId = await Contacts.removeContactAsync(context.state.contacts[0].id);
-
-	console.warn(contactId);
+	await Contacts.removeContactAsync(context.state.contacts[0].id);
+	await RemoveHelper(context)
+		? alert('Removed successfully')
+		: alert('Invalid Contact')
 
 };
 
---
+++ services/RemoveHelper.js
@@ -0,0 +1,13 @@
+import * as Contacts from 'expo-contacts';
+
+const RemoveHelper = async (context) => {
+	const { data } = await Contacts.getContactsAsync({
+		fields: [Contacts.Fields.FirstName, Contacts.Fields.LastName, Contacts.Fields.PhoneNumbers]
+	});
+
+	console.warn(data.length)
+
+	return data.length === context.state.contacts.length;
+};
+
+export default RemoveHelper;
